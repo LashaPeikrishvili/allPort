6a8af9fa5093c8877aaa03ddccadb39bd6c6487e
Lasha Peikrishvili <lasha.peikrishvili17b@geolab.edu.ge>
2022-07-13 02:45:25 +0400

    must fix search button

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ languages.addEventListener("click", function () {
     }
 
 })
+
+let search = document.querySelector(".search");
+
+let input = document.querySelector(".input");
+let img = document.querySelector(".img-search");
+let hasPlaceholder = false;
+
+img.addEventListener("click", function () {
+
+    input.classList.toggle("style")
+    if (hasPlaceholder == true) {
+        input.removeAttribute("placeholder");
+        hasPlaceholder = false;
+    } else {
+        input.setAttribute("placeholder", "search");
+        hasPlaceholder = true;
+    }
+
+})
