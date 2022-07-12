06adc3d6c81359e974f9b7e3069fc76606060755
Lasha Peikrishvili <lasha.peikrishvili17b@geolab.edu.ge>
2022-07-13 02:36:24 +0400

    flags are changing

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-// let port = document.querySelector(".allPort")
-// port.style.color = ("red")
-// for (let i = 0; i < 3; i++) {
-//     console.log(i)
-// }
+let usaFlag = document.querySelector(".usa");
+let geoFlag = document.querySelector(".geo");
+let languages = document.querySelector(".languages")
+let usaEn = document.querySelector(".usaEn");
+let geoGe = document.querySelector(".geoGe")
+languages.addEventListener("click", function () {
+    if (languages.value == 1) {
+        usaFlag.style.display = "block";
+        geoFlag.style.display = "none";
+    } else if (languages.value == 2) {
+        usaFlag.style.display = "none";
+        geoFlag.style.display = "block";
+    }
+
+})
